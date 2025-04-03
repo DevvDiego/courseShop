e5458bc9890f483de136818ca7bef146d5816b86
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-04-02 23:28:03 -0600

    Add basic nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,44 @@
 <script>
 	import '../app.css';
 	
+	let routes = [
+	
+		{"text":"Home", "url":"/"},
+		{"text":"Cursos", "url":"/courses"},
+	
+	]
+
+    let selectedRoute = $state("/"); // Inicialmente seleccionamos la ruta "/"
+    function selectRoute(url) {
+        selectedRoute = url;
+    }
+
 	let { children } = $props();
 </script>
 
+
+
+<nav class="w-full">
+	<ul class="w-full h-full flex items-center">
+	
+		{#each routes as route}
+
+			<li class="p-3 {selectedRoute === route.url ? 'selected' : ''}" >
+				<a href={route.url} onclick={() => selectRoute(route.url)}>
+					{route.text}
+				</a>
+			</li>
+
+		{/each}
+
+	</ul>
+</nav>
+
 {@render children()}
+
+
+<style>
+	.selected{
+		background-color: #35353d;
+	}
+</style>
