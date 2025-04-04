a76133f1fbbfecb3258a1a0b77edce937ad9e3d5
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-04-03 22:45:58 -0600

    Add courses page skeleton load and test data fetching

---
+++ src/lib/components/Course.svelte
@@ -0,0 +1,62 @@
+<script>
+
+
+    let {title, summary, startHour, src, alt, skeleton=null} = $props();
+</script>
+
+{#if skeleton}
+
+    <div class="w-48">
+        <div class="skeleton h-[200px] mb-2 rounded-2xl"></div>
+
+        <div>
+            <div class="skeleton p-4 mb-2 rounded-xl"></div>
+
+            <div class="skeleton p-4 rounded-xl"></div>
+        </div>
+    </div>
+
+{:else}
+    <div class="w-48">
+        
+        <div class="w-full mb-4">
+            <img class="w-full h-full" {src} {alt}>
+        </div>
+
+        <div>
+            <h1 class="w-full my-4">{title}</h1>
+
+            <p>
+                {summary}
+            </p>
+
+            <p class="text-xs mt-3">
+                Horario: {startHour}
+            </p>
+
+            <button class="bg-blue-900 mt-5 px-3 py-1 rounded-xl">
+                Registrarse
+            </button>
+        </div>
+    </div>
+{/if}
+
+<style>
+    
+    /* Estilo base para el contenedor */
+    .skeleton {
+        background: linear-gradient(90deg, #151515 25%, #303030 50%, #151515 75%);
+        background-size: 200% 100%;
+        animation: skeleton-animation 1.2s infinite;
+    }
+
+    /* Animación para el efecto de "esqueleto" */
+    @keyframes skeleton-animation {
+      0% {
+        background-position: 100% 0;
+      }
+      100% {
+        background-position: -100% 0;
+      }
+    }
+</style>
--- src/routes/courses/+page.svelte
+++ src/routes/courses/+page.svelte
@@ -1,3 +1,47 @@
+<script>
+    import Course from "$lib/components/Course.svelte";
+    import { onMount } from "svelte";
+
+    let courses = $state( null );
+    
+    onMount(()=>{
+
+        fetch("/courses.json")
+        .then(res=>{
+            return res.json();
+        })
+        .then(json=>{
+            // console.log(json);
+            courses = json;
+        })
+
+    })
+
+</script>
+
+
 <h1 class="text-3xl">
-    Cursos
+    asd
 </h1>
+
+<div class="flex flex-wrap items-center gap-12">
+
+    {#if courses}
+
+        {#each courses as course}
+
+            <Course title={course.name} summary={course.summary} startHour={course.startHour} src={"https://picsum.photos/200/200"} />
+
+        {/each}
+
+    {:else}
+
+        <Course skeleton={true} />
+        <Course skeleton={true} />
+        <Course skeleton={true} />
+        <Course skeleton={true} />
+        <Course skeleton={true} />
+
+    {/if}
+
+</div>
