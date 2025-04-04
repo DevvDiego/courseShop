135938e61dc1143c52eb529be62d58e15917def1
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-04-03 20:46:58 -0600

    add test data

---
+++ src/lib/data.js
@@ -0,0 +1,33 @@
+export const courses = [
+    
+        {
+            "name":"Reposteria",
+            "summary":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit, sunt!",
+            "startDate":"1/1/1997"
+        },   
+    
+        {
+            "name":"Lenguaje de señas",
+            "summary":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit, sunt!",
+            "startDate":"1/1/1997"
+        },   
+    
+        {
+            "name":"Corte y confección",
+            "summary":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit, sunt!",
+            "startDate":"1/1/1997"
+        },   
+    
+        {
+            "name":"Lecto-escritura",
+            "summary":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit, sunt!",
+            "startDate":"1/1/1997"
+        },   
+    
+        {
+            "name":"Preparación de alimentos y bebidas",
+            "summary":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit, sunt!",
+            "startDate":"1/1/1997"
+        },  
+ 
+]
