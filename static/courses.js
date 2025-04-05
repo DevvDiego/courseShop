95f863ba81d937439f9a76f60ea8ef79394bbdd8
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-04-05 13:56:39 -0600

    convert to exportable file for sveltekit load

--- static/courses.js
+++ static/courses.js
@@ -1,26 +1,31 @@
-[
+export const courses = [
     {
         "name": "Reposteria",
+        "url":"1",
         "summary": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit, sunt!",
         "startHour": "18:00"
     },
     {
         "name": "Lenguaje de señas",
+        "url":"2",
         "summary": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit, sunt!",
         "startHour": "18:00"
     },
     {
         "name": "Corte y confección",
+        "url":"3",
         "summary": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit, sunt!",
         "startHour": "18:00"
     },
     {
         "name": "Lecto-escritura",
+        "url":"4",
         "summary": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit, sunt!",
         "startHour": "18:00"
     },
     {
         "name": "Preparación de alimentos y bebidas",
+        "url":"5",
         "summary": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit, sunt!",
         "startHour": "18:00"
     }
