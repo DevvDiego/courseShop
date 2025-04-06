b25b9116377503c6e220cfe47cfa0a314f54aff6
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-04-05 18:10:41 -0600

    Add input for the form

---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,14 @@
+<script>
+
+    let { type, name, placeholder, text, class: Classname} = $props();
+</script>
+
+<div class={Classname}>
+    <label for={name} class="block mb-2 text-base font-bold">
+        {text}
+    </label>
+    <input {type} {placeholder} {name} class="
+        w-full p-3 px-4 text-sm
+        rounded-md border-[1px] border-zinc-700
+    ">
+</div>
