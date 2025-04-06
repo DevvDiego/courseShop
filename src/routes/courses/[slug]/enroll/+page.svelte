b55a352ffc360c2295c91354f62e9a38f901d829
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-04-06 12:12:23 -0600

    Add notification

--- src/routes/courses/[slug]/enroll/+page.svelte
+++ src/routes/courses/[slug]/enroll/+page.svelte
@@ -1,8 +1,38 @@
 <script>
     import Input from '$lib/components/Input.svelte';
+
+    let sendNotification = $state(false);
+    let notificationText = $state("");
+
+    function notificationTimeout(){
+        setTimeout(()=>{
+            sendNotification = false;
+        }, 15 * 1000) //mantener por 15 segundos
+    }
+
+    function getData(event){
+        event.preventDefault();
+
+        const form = event.target;
+        const data = new FormData(form);
+
+        console.log(data.get("email"));
+        console.log(data.get("name"));
+        
+        // Falta rectificar estado del formulario para mandar notificacion de error
+        notificationText = "Enviado correctamente" 
+        sendNotification = true;
+    }
+
 </script>
 
-<form class="max-w-md mx-auto my-24 bg-zinc-950 rounded-lg" action="#">
+<form onsubmit={getData} class="max-w-md mx-auto my-24 bg-zinc-950 rounded-lg" action="#">
+    {#if sendNotification}
+        <div class="w-full bg-green-800 py-2 text-center text-xl font-bold">
+            {notificationText}
+        </div>
+    {/if}
+    
     <h1 class="py-2 text-center text-lg font-medium w-full">Inscribete ahora</h1>
 
     <div class="p-2 px-5 flex flex-col">
